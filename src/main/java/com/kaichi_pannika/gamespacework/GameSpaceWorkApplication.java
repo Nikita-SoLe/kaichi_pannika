55fdf0fa108d156cb6b11b90e7261b49d23c822e
Nikita Pantyuhin <nikita.pantyuhin@gmail.com>
2024-06-14 22:56:44 +0300

    0.1

---
+++ src/main/java/com/kaichi_pannika/gamespacework/GameSpaceWorkApplication.java
@@ -0,0 +1,13 @@
+package com.kaichi_pannika.gamespacework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSpaceWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSpaceWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaichi_pannika/gamespacework/GameSpaceWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaichi_pannika.gamespacework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSpaceWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
